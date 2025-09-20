bd1fa81ae0020eb7f676cf17a1722e3f3057a214
ValeriiTsoi <valerii.tsoi@yahoo.com>
2025-09-20 22:29:14 +0200

    init: website (Laravel + i18n + CI/CD for Apache)

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,18 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\App;
 use App\Http\Controllers\PageController;
 
-Route::get('/', function () { return redirect('/en'); });
+Route::get('/', fn () => redirect('/en'));
 
-Route::group(['prefix' => '{locale}', 'where' => ['locale' => 'en|uk|es|ru']], function () {
-    Route::middleware(function ($request, $next) {
-        $locale = $request->route('locale'); App::setLocale($locale); return $next($request);
-    })->group(function () {
-        Route::get('/', [PageController::class, 'home'])->name('home');
-        Route::get('/about', [PageController::class, 'about'])->name('about');
-        Route::get('/projects', [PageController::class, 'projects'])->name('projects');
-        Route::get('/hobby', [PageController::class, 'hobby'])->name('hobby');
-        Route::get('/contact', [PageController::class, 'contact'])->name('contact');
-    });
+Route::group([
+    'prefix' => '{locale}',
+    'where' => ['locale' => 'en|uk|es|ru'],
+    'middleware' => 'setLocale',
+], function () {
+    Route::get('/', [PageController::class, 'home'])->name('home');
+    Route::get('/about', [PageController::class, 'about'])->name('about');
+    Route::get('/projects', [PageController::class, 'projects'])->name('projects');
+    Route::get('/hobby', [PageController::class, 'hobby'])->name('hobby');
+    Route::get('/contact', [PageController::class, 'contact'])->name('contact');
 });
